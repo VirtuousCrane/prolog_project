450fc9fba82e41edb0a401025557e3f145e8bbad
VirtuousCrane <ray.daroonpat.2020@gmail.com>
2022-11-09 17:00:11 +0700

    Init

---
+++ src/main/java/com/example/prologio/PrologioApplication.java
@@ -0,0 +1,13 @@
+package com.example.prologio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrologioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrologioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/prologio/PrologioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prologio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrologioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
